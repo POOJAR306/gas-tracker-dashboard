1295a6f912670bc51da076cd7a63a13543d91e61
Poo306 <poojarp948@gmail.com>
2025-07-20 08:08:04 +0530

    removed usd, gasData, [txAmount] in Home() which was seclared twice

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,6 @@ export default function Home() {
 
   return () => clearInterval(interval)
   }, []);
-  const [txAmount, setTxAmount] = useState(0.5);
-  const gasData = useGasStore((state) => state.chains);
-  const usd = useGasStore((state) => state.usdPrice);
-  const gasData = useGasStore((state) => state.gasData);
   return (
     <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
       <h1> Real-Time Cross-Chain Gas Tracker</h1>
