361101012757a91571e77bc8b296aca9bc226823
Poo306 <poojarp948@gmail.com>
2025-07-20 07:37:41 +0530

    Added pre tag in index.tsx and added semicolons in few places

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,16 @@ import { startGasTracking } from '../utils/gasFetcher'
 import { useGasStore } from '../store/useGasStore'  
 
 export default function Home() {
-  const gas = useGasStore((state) => state.gasData)
   useEffect(() => {
-    startGasTracking()
-  }, [])
+    startGasTracking();
+  }, []);
 
+  const gasData = useGasStore((state) => state.gasData);
   return (
     <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
-    <h1>⛽ Real-Time Cross-Chain Gas Tracker</h1>
-    <p>Fetching live gas prices from Ethereum, Polygon, and Arbitrum...</p>
-
-    <ul>
-      <li>Ethereum: {gas.ethereum?.toFixed(2) || 'Loading...'} gwei</li>
-      <li>Polygon: {gas.polygon?.toFixed(2) || 'Loading...'} gwei</li>
-      <li>Arbitrum: {gas.arbitrum?.toFixed(2) || 'Loading...'} gwei</li>
-    </ul>
+      <h1>⛽ Real-Time Cross-Chain Gas Tracker</h1>
+      <p>Fetching live gas prices from Ethereum, Polygon, and Arbitrum...</p>
+      <pre>{JSON.stringify(gasData, null, 2)}</pre>
     </main>
-  )
+  );
 }
