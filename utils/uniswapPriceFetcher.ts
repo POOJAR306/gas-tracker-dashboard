1102fe44016925cdfe76e46e023a6e1fb8bfe04f
Poo306 <poojarp948@gmail.com>
2025-07-20 08:26:40 +0530

    copied the line 13 and pasted outside fetchPrice in line 9

--- utils/uniswapPriceFetcher.ts
+++ utils/uniswapPriceFetcher.ts
@@ -2,15 +2,15 @@ import { JsonRpcProvider, ethers } from 'ethers'
 import { useGasStore } from '../store/useGasStore'
 import type { EventLog } from 'ethers'
 
-const UNISWAP_POOL_ADDRESS = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640' // ETH/USDC
+const UNISWAP_POOL = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640' // ETH/USDC
 const UNISWAP_ABI = [
   'event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)'
 ]
+const provider = new JsonRpcProvider( `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`)
+
 
 export const fetchEthPrice = async () => {
-  const provider = new JsonRpcProvider( `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`)
-
-  const contract = new ethers.Contract(UNISWAP_V3_POOL_ADDRESS, UNISWAP_ABI, provider)
+  const contract = new ethers.Contract(UNISWAP_POOL, UNISWAP_ABI, provider)
 
   const latestBlock = await provider.getBlockNumber()
   const fromBlock = latestBlock - 100
